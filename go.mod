0a67b93be802c396ad8e1132a658290879f709b0
az58740 <zeinalzadehali2@gmail.com>
2024-09-18 20:01:49 +0330

    code generate

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/az58740/connect-go-web-proto
 
 go 1.22.0
+
+require (
+	connectrpc.com/connect v1.16.2 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
--- golang/gen/users/v1/usersv1connect/users.connect.go
+++ golang/gen/users/v1/usersv1connect/users.connect.go
@@ -8,7 +8,7 @@ import (
 	connect "connectrpc.com/connect"
 	context "context"
 	errors "errors"
-	v1 "github.com/az58740/connect-go-web-proto/proto/users/v1"
+	v1 "github.com/az58740/connect-go-web-proto/users/v1"
 	http "net/http"
 	strings "strings"
 )
